192ecfe92667eb2afc5d6f7576396e439106ac0b
Vaquero Mena <evsei.vladmr@gmail.com>
2026-05-26 13:51:03 +0200

    Initial Commit

---
+++ MyNetDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNetDiary
+//
+//  Created by Uladzimir Yausei on 26/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
